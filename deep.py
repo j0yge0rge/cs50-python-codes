affa86b97f6c43df6417cbd081a6ac4790f0555f
j0yge0rge <149228553+j0yge0rge@users.noreply.github.com>
2025-01-27 13:40:59 +0200

    Add files via upload

---
+++ deep.py
@@ -0,0 +1,5 @@
+x = input("what is the answer to the greatest question of life , the universe and everything?")
+if x.strip() == "42" or x.lower() =="forty-two" or x.lower() == "forty two":
+     print("Yes")
+else:
+     print("No")
